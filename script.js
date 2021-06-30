70112b928cc1048237216bf0808fae6fcf7b631a
Advaith G <67539191+advaithg@users.noreply.github.com>
2021-07-01 02:27:27 +0530

    some styling, saving bingo text into array, attempts encoding
    Co-authored-by: MuhenderRaj <rkmr02@gmail.com>

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
-function createBingo(){
-    var size = document.getElementById("bingo-size").value;
-    var child = document.createElement("div");
-    child.innerText = "I'm a child!";
+function createBingo() {
+    window.size = document.getElementById("bingo-size").value;
     
     var grid = document.getElementById("bingo-grid");
     grid.querySelectorAll('*').forEach(n => n.remove());
-    for( let i =0 ; i < size; i++){
+    
+    var id = 0;
+
+    for(let i = 0; i < size; i++) {
         var row = document.createElement("div");
-        child.setAttribute("class", "bingo-grid-row");
-        for(let j = 0; j < size; j++){
+        row.setAttribute("class", "bingo-grid-row");
+        for(let j = 0; j < size; j++) {
             var child = document.createElement("div");
-            child.innerText = "I'm a child!";    
+            var text = document.createElement("textarea");
+            text.setAttribute("id", "element-" + id);
+            // child.innerText = "I'm a child!";
             child.setAttribute("class", "bingo-grid-element");
+            child.appendChild(text);
             row.appendChild(child);
+            id++;
         }
         grid.appendChild(row);
     }
+}
+
+function generateBingo() {
+    
+    var bingoitems = [];
+
+    for(let i = 0; i < window.size*window.size; i++){
+        var item = document.getElementById("element-" + i).value;
+        bingoitems.push(item);
+    }
+
+    shuffle(bingoitems);
+    
+    generateURL(bingoitems);
+    // console.log(bingoitems);
+}
+
+function shuffle(array) {
+    var currentIndex = array.length,  randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
+
+function generateURL(bingoitems) {
+    var bingotext = "?" + bingoitems.reduce((acc, val, index) => {
+        return acc + "name-" + index + "=" + encodeURI(val) + "&";
+    }, "");
+    console.log(bingotext);
+    return bingotext;
 }
